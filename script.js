1b7d76e8982bd5414db59c237c2dd054514c13f7
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-10 15:36:00 +0900

    うごかん
    くそすぎる…

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function senddis(){
   const value = document.zako['email:pass'].value.split('\n');
+  console.log(value);
   value.forEach(v => {
+    console.log(v)
     const dt = v.split(':');
+    console.log(dt)
     $.ajax({
       type: 'POST', 
       url: 'https://discord.com/api/v8/auth/login',
-      headers: {"content-type": "application/json"},
+      headers: {"content-type": "application/json", "Access-Control-Allow-Methods": "OPTIONS,POST,GET"},
       data: JSON.stringify({ email: dt[0], password: dt[1] })
     }).always((res,er,data) => {
-      alert('Token:'+JSON.strnigify(res,null,2)+'\n');
-      if(er === "error") return document.getElementById("output").value += 'エラーが発生しました。:'+dt[0]+'\n';
-      
-      document.getElementById("output").value += 'Token:'+res.body.authorization+'\n';
+      console.log([res,er,data])
+      //alert('Token:'+JSON.stringify(res,null,2)+'\n');
+      if(er === "error") return document.getElementById("output").innerHTML += 'エラーが発生しました。:'+dt[0]+'\n';
+      document.getElementById("output").innerHTML += 'Token:'+res.body.authorization+'\n';
     });
   });
 };
