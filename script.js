c509de7e756141ac50f9f8643df9a773fe835c79
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-11 16:44:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const sp = window.superagent;
+"use strict";
+var sp = window.superagent;
 function senddis(){
-  const value = document.zako['email:pass'].value.split('\n');
-  value.forEach(v => {
-    const dt = v.split(':');
+  var value = document.zako['email:pass'].value.split('\n');
+  value.forEach(function (v){
+    var dt = v.split(':');
     sp
     .post('https://discord.com/api/v8/auth/login')
     .send({email:dt[0],password:dt[1]})
     .end(function(er,re){
       if(!re.ok) return appendlog('エラーが発生しました。 '+dt[0]+'\n');
-      const body = re.body;
+      var body = re.body;
       appendlog(body.token);
     });
   });
-};
+}
+
 function appendlog(strings){
   document.getElementById("output").innerHTML += '[TokenGetter-with-web]: '+strings;
-};
+}
+
 function cleardis(){
   document.zako['email:pass'].value = '';
-};
+}
+
 function copydis(){
   document.zako['email:pass'].select();
   document.execCommand('Copy');
-};
+}
+
 function copytoken(){
   document.getElementById('output').select();
   document.execCommand('Copy');
-};
+}
