ffac7788871b167d8687c39bf4d018d865c4c354
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-30 09:00:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ function senddis(){
   value.forEach(function (v){
     var dt = v.split(':');
     sp
-    .post('https://discord.com/api/v8/auth/login')
-    .set("Origin","https://discord.com")
+    .post("https://discord.com/api/v8/auth/login")
+    .set("origin", "https://discord.com")
+    .set("user-agent", "DiscordBot (https://discordapp.com, 2.0)")
+    .set("accept", "*/*")
+    .set("content-type", "application/json")
+    .set("referrer", "https://discord.com/login")
+    .set("sec-ch-ua": "DiscordBot (https://discordapp.com, 2.0)")
+    .set("sec-ch-ua-mobile", "?0")
+    .set("sec-fetch-dest", "empty")
+    .set("sec-fetch-mode", "cors")
+    .set("sec-fetch-site", "same-origin")
     .send({email:dt[0],password:dt[1]})
     .end(function(er,re){
       if(!re.ok) appendlog('エラーが発生しました。 '+dt[0]);
       var body = re.body;
-      appendlog(re);
-      appendlog(JSON.stringify(re,null,2))
-      appendlog(er)
-      appendlog(body.token)
-      appendlog(body)
     });
   });
 }
